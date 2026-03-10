f0cc0bf2969234f15caeb9da44426209d67c5b3a
MarsPatrick <36931248+MarsPatrick@users.noreply.github.com>
2026-03-10 16:51:25 -0300

    adapted code

--- scripts/get_updates.sh
+++ scripts/get_updates.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
 ########################333333##################
 ##                                            ##
 ## This script has not been tested yet.       ##
 ## Waiting for a new server version to do it. ##
 ##                                            ##
 ########################333333##################
-
-DOCKER_IMAGE="danixu86/project-zomboid-dedicated-server"
+PODMAN_IMAGE="danixu86/project-zomboid-dedicated-server"
 PZ_URL_WEB="https://projectzomboid.com/blog/"
 PZ_URL_FORUM="https://theindiestone.com/forums/index.php?/forum/35-pz-updates/"
 BUILD_UNSTABLE_VERSIONS=true
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}/../"
 
@@ -27,11 +24,9 @@ cd "${SCRIPT_DIR}/../"
 function versionCompare(){
   A_LENGTH=`echo -n $1|sed 's/[^\.]*//g'|wc -m`
   B_LENGTH=`echo -n $2|sed 's/[^\.]*//g'|wc -m`
-
   REVERSE=0
   A=""
   B=""
-
   if [ ${B_LENGTH} -gt ${A_LENGTH} ]; then
     A=$2
     B=$1
@@ -40,13 +35,10 @@ function versionCompare(){
     A=$1
     B=$2
   fi
-
   CURRENT=1
   A_NUM=`echo -n $A|cut -d "." -f${CURRENT}`
-
   while [ "${A_NUM}" != "" ]; do
     B_NUM=`echo -n $B|cut -d "." -f${CURRENT}`
-
     if [ "$B_NUM" == "" ] || [ $A_NUM -gt $B_NUM ]; then
       if [ $REVERSE == 1 ]; then echo -1; else echo 1; fi
       return 0;
@@ -54,7 +46,6 @@ function versionCompare(){
       if [ $REVERSE == 1 ]; then echo 1; else echo -1; fi
       return 0;
     fi
-
     CURRENT=$((${CURRENT} + 1))
     A_NUM=`echo -n $A|cut -d "." -f${CURRENT}`
   done
@@ -66,12 +57,12 @@ function versionCompare(){
 ## Checking the latest built version    ##
 ##                                      ##
 ##########################################
-LATEST_IMAGES=`curl -L -s "https://registry.hub.docker.com/v2/repositories/${DOCKER_IMAGE}/tags?page_size=1024" | jq  '.results[]["name"]' | grep -iv "latest" | sort`
+LATEST_IMAGES=`curl -L -s "https://registry.hub.docker.com/v2/repositories/${PODMAN_IMAGE}/tags?page_size=1024" | jq  '.results[]["name"]' | grep -iv "latest" | sort`
 # Get the latest stable version
 LATEST_IMAGE_STABLE_VERSION=`echo "${LATEST_IMAGES}" | grep -i "release" | tail -n1 | grep -oE "[0-9]+\.[0-9]+\.[0-9]+|[0-9]+\.[0-9]+" | sed 's/"//g'`
 # Get the latest unstable version.
 LATEST_IMAGE_UNSTABLE_VERSION=`echo "${LATEST_IMAGES}" | grep -i "unstable" | tail -n1 | grep -oE "[0-9]+\.[0-9]+\.[0-9]+|[0-9]+\.[0-9]+" | sed 's/"//g'`
-echo "Latest docker images version:"
+echo "Latest image versions:"
 echo -e "\tRELEASE: ${LATEST_IMAGE_STABLE_VERSION}"
 echo -e "\tUNSTABLE: ${LATEST_IMAGE_UNSTABLE_VERSION}"
 
@@ -93,7 +84,6 @@ grep -oE "[0-9]+\.[0-9]+\.[0-9]+|[0-9]+\.[0-9]+" | sort | uniq)
 LATEST_FORUM_UNSTABLE_VERSIONS=$(echo "${FORUM_DATA}" | \
 grep -oPi "[0-9]{2,3}\.[0-9]{1,2}(\.[0-9]{1,2})? ($UNSTABLE_TITLES)" | \
 grep -oE "[0-9]+\.[0-9]+\.[0-9]+|[0-9]+\.[0-9]+" | sort | uniq)
-
 # Sometimes a pinned post hiddens the latest version, so all versions will be checked
 LATEST_FORUM_STABLE_VERSION=0.0.0
 for version in $LATEST_FORUM_STABLE_VERSIONS; do
@@ -109,7 +99,6 @@ for version in $LATEST_FORUM_UNSTABLE_VERSIONS; do
     LATEST_FORUM_UNSTABLE_VERSION=$version
   fi
 done
-
 echo -e "\n\nLatest forum versions:"
 echo -e "\tRELEASE: ${LATEST_FORUM_STABLE_VERSION}"
 echo -e "\tUNSTABLE: ${LATEST_FORUM_UNSTABLE_VERSION}"
@@ -122,23 +111,19 @@ echo -e "\tUNSTABLE: ${LATEST_FORUM_UNSTABLE_VERSION}"
 # Texts to search on the webpage
 STABLE_TEXT="Stable Build"
 UNSTABLE_TEXT="IWBUMS Beta"
-
 # Extract the versions from the website (sometimes it is outdated, that is why I check first the forum)
 WEBPAGE_DATA=`curl "${PZ_URL_WEB}" 2>/dev/null`
 LATEST_WEBPAGE_STABLE_VERSION=`echo "${WEBPAGE_DATA}" | grep -i "${STABLE_TEXT}" | head -n1 | cut -d ":" -f2 | awk '{print $1}'`
 LATEST_WEBPAGE_UNSTABLE_VERSION=`echo "${WEBPAGE_DATA}" | grep -i "${UNSTABLE_TEXT}" | head -n1 | cut -d ":" -f2 | awk '{print $1}'`
-
 echo -e "\n\nLatest website versions:"
 echo -e "\tRELEASE: ${LATEST_WEBPAGE_STABLE_VERSION}"
 echo -e "\tUNSTABLE: ${LATEST_WEBPAGE_UNSTABLE_VERSION}"
-
 
 ##################################
 ##                              ##
 ## Building the required images ##
 ##                              ##
 ##################################
-
 LATEST_STABLE_VERSION=""
 LATEST_STABLE_VERSION_COMPARE=$(versionCompare ${LATEST_FORUM_STABLE_VERSION} ${LATEST_WEBPAGE_STABLE_VERSION})
 if [ $LATEST_STABLE_VERSION_COMPARE == -1 ]; then
@@ -146,7 +131,6 @@ if [ $LATEST_STABLE_VERSION_COMPARE == -1 ]; then
 else
   LATEST_STABLE_VERSION=$LATEST_FORUM_STABLE_VERSION
 fi
-
 LATEST_UNSTABLE_VERSION=""
 LATEST_UNSTABLE_VERSION_COMPARE=$(versionCompare ${LATEST_FORUM_UNSTABLE_VERSION} ${LATEST_WEBPAGE_UNSTABLE_VERSION})
 if [ $LATEST_UNSTABLE_VERSION_COMPARE == -1 ]; then
@@ -154,7 +138,6 @@ if [ $LATEST_UNSTABLE_VERSION_COMPARE == -1 ]; then
 else
   LATEST_UNSTABLE_VERSION=$LATEST_FORUM_UNSTABLE_VERSION
 fi
-
 echo -e "\n\nDetected real latest versions (forum and website):"
 echo -e "\tRELEASE: ${LATEST_STABLE_VERSION}"
 echo -e "\tUNSTABLE: ${LATEST_UNSTABLE_VERSION}"
@@ -162,19 +145,20 @@ echo -e "\tUNSTABLE: ${LATEST_UNSTABLE_VERSION}"
 if [ ${BUILD_UNSTABLE_VERSIONS} == true ]; then
   echo -e "\n\n****************************************************************************"
   echo "The unstable image build is enabled. Checking latest version..."
-
   NEW_VERSION=$(versionCompare ${LATEST_UNSTABLE_VERSION} ${LATEST_IMAGE_UNSTABLE_VERSION})
-
   if [ "${LATEST_IMAGE_UNSTABLE_VERSION}" == "" ] || [ $NEW_VERSION == 1 ]; then
     echo -e "\n\nA new version of the unstable server was detected ($LATEST_UNSTABLE_VERSION). Creating the new image...\n"
-
-    docker build --compress --no-cache --build-arg STEAMAPPBRANCH=unstable -t ${DOCKER_IMAGE}:latest-unstable -t ${DOCKER_IMAGE}:${LATEST_UNSTABLE_VERSION}-unstable .
-    docker push ${DOCKER_IMAGE}:${LATEST_UNSTABLE_VERSION}-unstable
-    docker push ${DOCKER_IMAGE}:latest-unstable
+    # Podman: --compress no existe, se usa --squash o simplemente se omite.
+    # --no-cache y --build-arg funcionan igual que en Docker.
+    podman build --squash --no-cache --build-arg STEAMAPPBRANCH=unstable \
+      -t ${PODMAN_IMAGE}:latest-unstable \
+      -t ${PODMAN_IMAGE}:${LATEST_UNSTABLE_VERSION}-unstable .
+    podman push ${PODMAN_IMAGE}:${LATEST_UNSTABLE_VERSION}-unstable
+    podman push ${PODMAN_IMAGE}:latest-unstable
   elif [ $NEW_VERSION == 0 ]; then
     echo -e "\n\nThere is no new unstable version of the Zomboid server\n\n"
   elif [ $NEW_VERSION == -1 ]; then
-    echo -e "\n\nServer unstable version (${LATEST_UNSTABLE_VERSION}) is lower than latest docker version (${LATEST_IMAGE_UNSTABLE_VERSION})... Please, check this script because maybe is not working correctly\n\n"
+    echo -e "\n\nServer unstable version (${LATEST_UNSTABLE_VERSION}) is lower than latest image version (${LATEST_IMAGE_UNSTABLE_VERSION})... Please, check this script because maybe is not working correctly\n\n"
   else
     echo -e "\n\nThere was an unknown error checking the unstable version.\n\n"
   fi
@@ -185,20 +169,23 @@ fi
 echo -e "\n\n****************************************************************************"
 echo "Checking the latest stable version..."
 NEW_VERSION=$(versionCompare ${LATEST_STABLE_VERSION} ${LATEST_IMAGE_STABLE_VERSION})
-
 if [ "${LATEST_IMAGE_STABLE_VERSION}" == "" ] || [ $NEW_VERSION == -1 ]; then
-  echo -e "\n\nA new version of the unstable server was detected ($LATEST_STABLE_VERSION). Creating the new image...\n"
-
-  docker build --compress --no-cache -t ${DOCKER_IMAGE}:latest -t ${DOCKER_IMAGE}:latest-release -t ${DOCKER_IMAGE}:${LATEST_SERVER_VERSION}-release .
-  docker push ${DOCKER_IMAGE}:${LATEST_SERVER_VERSION}-release
-  docker push ${DOCKER_IMAGE}:latest-release
-  docker push ${DOCKER_IMAGE}:latest
+  echo -e "\n\nA new version of the stable server was detected ($LATEST_STABLE_VERSION). Creating the new image...\n"
+  # Podman: misma lógica que arriba. Además se corrige el bug del original:
+  # usaba LATEST_SERVER_VERSION (no definida) en lugar de LATEST_STABLE_VERSION.
+  podman build --squash --no-cache \
+    -t ${PODMAN_IMAGE}:latest \
+    -t ${PODMAN_IMAGE}:latest-release \
+    -t ${PODMAN_IMAGE}:${LATEST_STABLE_VERSION}-release .
+  podman push ${PODMAN_IMAGE}:${LATEST_STABLE_VERSION}-release
+  podman push ${PODMAN_IMAGE}:latest-release
+  podman push ${PODMAN_IMAGE}:latest
 elif [ $NEW_VERSION == 0 ]; then
-  echo -e "\n\nThere is no new unstable version of the Zomboid server\n\n"
+  echo -e "\n\nThere is no new stable version of the Zomboid server\n\n"
 elif [ $NEW_VERSION == 1 ]; then
-  echo -e "\n\nServer unstable version (${LATEST_STABLE_VERSION}) is lower than latest docker version (${LATEST_IMAGE_STABLE_VERSION})... Please, check this script because maybe is not working correctly\n\n"
+  echo -e "\n\nServer stable version (${LATEST_STABLE_VERSION}) is lower than latest image version (${LATEST_IMAGE_STABLE_VERSION})... Please, check this script because maybe is not working correctly\n\n"
 else
-  echo -e "\n\nThere was an unknown error checking the unstable version.\n\n"
+  echo -e "\n\nThere was an unknown error checking the stable version.\n\n"
 fi
 echo "****************************************************************************"
 echo -e "\n\n"
